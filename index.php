9a82e669973d389acc4daeebe5eedc46a5b28038
Cesar Velez <velezcd@gmail.com>
2012-02-23 22:49:42 -0500

    updated eric branch and added a few things to get us started with the main page. Like a global.css for our styles, fonts, colors etc

--- index.php
+++ index.php
@@ -3,12 +3,42 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-<title>Wolfpack.com All You need to know about dogs</title>
-</head>
+<title>Wolfpack's Awesome Website All You need to know about dogs</title>
+<!-- MetaData about data... -->
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta http-equiv="imagetoolbar" content="no" />
+        <meta name="author" content="Wolfpack" />
+        <meta name="copyright" content="Wolfpack" />
+        <meta name="company" content="Wuff, LLC" />
+        <meta name="revisit-after" content="3 days" />
+		<!-- No more meta please -->
+		
+			<!-- CSS global -->
+			<style type="text/css">
+				@import url(./css/global.css);
+			</style>
+			
+			<!-- Favicon is nice-->
+			<link rel="shortcut icon" href="./pics/favicon.ico" type="image/x-icon" />
 
+</head>
+<!-- 
+Start of Bodybody {
+
+-->
 <body>
 <h1> Wolfpack.com - All You Need To Know About Dogs</h1>
 <h2>Under Construction...</h2>
+
+<!-- Site Navigation Menu -->
+<ul class="navbar">
+	<li><a href="index.html">Home</a></li>
+	<li><a href="./vrsoftech/food.html">Dog Food</a>
+	<li><a href="./404.html">Popular Breeds</a></li>
+	<li><a href="./phpExamples/about.html">About</a></li>
+</ul>
+
+<!-- END-------------------------------------------------------- -->
 </body>
 
 </html>
