3fc098a827cd9cf351d6d85006a248f094cd8586
Cesar Velez <velezcd@gmail.com>
2012-04-07 16:44:05 -0400

    added some more code to foods.js

--- site/js/foods.js
+++ site/js/foods.js
@@ -24,7 +24,74 @@ $(document).ready( function() {
 		$(this).animate( {fontSize:"24px"}, 250 );
 	}); 
 	
+	/*Food SELECTION*/
 	
+	function populateFoods ( foodtype ) { /*letter = foodtype*/
+		var html = "";
+		var regex = null;
+		var both = false;
+		
+		if ( foodtype == "Good" ) {
+			regex = new RegExp("^true");
+			//alert(regex);
+		}
+		else if ( foodtype == "Bad") {
+			regex = new RegExp("^false");
+			//alert(regex);
+		//var both = foodtype=="Both";
+		}
+		else {
+			var both = true;
+			regex = new RegExp("*");	
+		}
+		//alert(both);
+		
+		$.each(foodsdata, function (key, val)
+		{
+			//regex = new RegExp("^true");
+			doAdd = val.isGood.match(regex);
+			if (doAdd || both) html += "<div id='"+key+"' class='foodItem'><div class='foodItemText'>"+val.name+"</div></div>";
+		});
+		
+		$("#foodList").html( html );
+		
+		$("div.foodItem:first").trigger("click");
+	}
+	
+	/*EVENTS*/
+		
+	//POPULATE THE Food SELECTION AREA
+	$("div.foodtype").bind( "click", function() {
+		if (! $(this).hasClass("div.foodtype_selected") ) {
+			$("div.foodtype:*").removeClass("foodtype_selected");
+			$(this).addClass("foodtype_selected");
+			
+			populateFoods( $(this).html() );
+		}
+	});
+	
+	//POPULATE THE FOOD CONTENT AREA
+	$("div.breedItem").live( "click", function() {
+		var id = $(this).attr("id");
+		
+		$("div#breedContent").empty().load( "./views/breed.html", function() {
+			
+			aBreed = breeds[id];
+			
+			$.each( aBreed, function (key, val) {
+				$("#"+key).html( val );
+			});
+			
+		});
+		
+		$("div.breedItem:*").removeClass("breedItem_selected");
+		$(this).addClass("breedItem_selected");
+		
+	});
+	
+	//The pages default selections.
+	
+	$("div#any").trigger("click");
 
 	
 });
