467b0c7dcadab49b72fb000a834e2dcbc4db03db
Eric Hunter <ejames788@gmail.com>
2012-04-08 12:19:47 -0400

    fixed weird equal signs

--- site/js/site.js
+++
@@ -1,8 +0,0 @@
-// JavaScript Document
-
-$(document).ready( function() {
-	// load the header dynamically so it doesn't need to recur on each page.
-	$("div#headerbg").load("./views/header.html");
-	
-	
-});
