676089364d80d9b2278f4073bcd28820a1fd78d1
Eric Hunter <ejames788@gmail.com>
2012-04-08 12:04:49 -0400

    Integrated changes

---
+++ site/js/foods.js
@@ -0,0 +1,82 @@
+/**
+  *AUTHOR: Cesar Velez
+  *7 April 2012
+*/
+
+$(document).ready( function() {
+	
+	//Food: Good - Bad - Both PANEL  -- not needed for foods page
+	var char = 0;
+	var html = "<div id='both' class='foodtype' style='margin-left:10px'>Both</div> - ";
+	
+	//for ( var inc='A'.charCodeAt(0); inc <= 'Z'.charCodeAt(0); inc++  ) {
+		
+		html += "<div class='foodtype'>Good</div> - ";
+		html += "<div class='foodtype'>Bad</div>";
+	
+	
+	$("div.goodbadPanel").html( html );
+	
+	$("div.foodtype").hover( function() {
+		$(this).animate( {fontSize:"30px"}, 250 );
+	},
+	function() {
+		$(this).animate( {fontSize:"24px"}, 250 );
+	}); 
+	
+	/*Food SELECTION*/
+	
+	function populateFoods ( foodtype ) { /*letter = foodtype*/
+		var html = "";
+        var both = foodtype == "Both";
+		var isGood = foodtype == "Good";
+
+		//alert(both);
+		$.each(foodsdata, function (key, val)
+		{
+                    //NOTE: removed regex because json will automatically treat true false like a boolean versus string.
+                    doAdd = val.isGood == isGood;
+                    if (doAdd || both) html += "<div id='"+key+"' class='foodItem'><div class='foodItemText'>"+val.name+"</div></div>";
+		});
+		
+		$("#foodList").html( html );
+		
+		$("div.foodItem:first").trigger("click");
+	}
+	
+	/*EVENTS*/
+		
+	//POPULATE THE Food SELECTION AREA
+	$("div.foodtype").bind( "click", function() {
+		if (! $(this).hasClass("div.foodtype_selected") ) {
+			$("div.foodtype:*").removeClass("div.foodtype_selected");
+			$(this).addClass("div.foodtype_selected");
+			populateFoods( $(this).html() );
+		}
+	});
+	
+	//POPULATE THE FOOD CONTENT AREA
+	$("div.foodItem").live( "click", function() {
+		var id = $(this).attr("id");
+		
+		$("div#foodContent").empty().load( "./views/afood.html", function() {
+			
+			aFood = foodsdata[id];
+			
+			$.each( aFood, function (key, val) {
+				$("#"+key).html( val );
+			});
+			
+		});
+		
+		$("div.foodItem:*").removeClass("foodItem_selected");
+		$(this).addClass("foodItem_selected");
+		
+	});
+	
+	//The pages default selections.
+	
+	$("div#both").trigger("click");
+
+	
+});
---
+++ src/JsonMaker/JsonMaker/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) // CREATE JSON clicked - this will generate json object 
+        {
+            string filepath = jsonfile.Text;
+            int dirends = filepath.LastIndexOf(@"\");
+            string dirpath = filepath.Substring(0,++dirends);
+
+            if (Directory.Exists(dirpath))
+            {
+                patherrorlbl.Visible = false;
+
+                //string jsonBreed;
+                // creates random 4-digit id
+                int seed = DateTime.Now.Second;
+                var _random = new Random(seed);
+                int id = _random.Next(1000, 9999);
+                idTxtbx.Text = id.ToString();
+                string lifespan = lifespanstart.Text + "-" + lifespanend.Text;
+
+                string name = breedname.Text;
+                string orig = origin.Text; 
+                string grp = group.Text;
+                string wgt = weight.Text; 
+                string tmpr = temper.SelectedItem.ToString();
+                string desc = breedNotes.Text;
+                string pic = picfilename.Text;
+                //string str = "{ \n\t\"id\" : {0}, \n\t\"name\" : \"{1}\", \n\t\"origin\" : \"{2}\", \n\t\"group\" : \"{3}\" , \n\t\"weight\" : \"{4}\", \n\t\"temperament\" : \"{5}\", \n\t\"lifespan\" : \"{6}\", \n\t\"description\" : \"{7}\", \n\t\"picfilename\" : \"{8}\", ";
+                //building json object string
+
+                // obj = "{ "; curlies were the effin prob all along. Escape them using {{ and }}
+
+                string obj = string.Format("\n{{ \n\t\"id\" : {0}, \n\t\"name\" : \"{1}\", \n\t\"origin\" : \"{2}\", \n\t\"group\" : \"{3}\" , \n\t\"weight\" : \"{4}\", \n\t\"temperament\" : \"{5}\", \n\t\"lifespan\" : \"{6}\", \n\t\"description\" : \"{7}\", \n\t\"picfilename\" : \"{8}\",", id, name, orig, grp, wgt, tmpr, lifespan, desc, pic);
+
+                //adds health sub-object if it has health issues
+                if (HasHealthIssues.SelectedItem.ToString() == "True")
+                {
+                    obj += string.Format("\n\t\"hasHealthIssue\": true, \n\t\"healthIssue\" : \n\t{{ \n\t\"issue\":\"{0}\"  \n\n\t}}, \n\n", healthIssue.Text);
+                }
+                else obj += "\n\t\"hasHealthIssue\": false, \n\t\"healthIssue\" : null, ";
+
+                if (HasTrainingTips.SelectedItem.ToString() == "True")
+                {
+                    obj += string.Format(" \n\t\"hasTrainingTip\": true, \n\t\"trainingTip\" : \n\n\t{{ \n\t\"tip\":\"{0}\"  \n\t}}, \n", TrainingTips.Text);
+                }
+                else
+                {
+                    obj += " \n\t\"hasTrainingTip\": false, \n\t\"trainingTip\" : null, ";
+                }
+
+                obj += string.Format("\n\t\"height\" : \"{0}\" ",heightTxtbox.Text);
+
+                obj += "\n\n" + @"}"; //last closing bracket
+
+                writeJson(obj, filepath);
+            }
+            else { patherrorlbl.Visible = true; return; }
+
+        }//end of button click
+
+        private static void writeJson( string str, string path)
+        {
+            using (StreamWriter outputfile = new StreamWriter(path, true))
+            {
+                //writes/appends each url into a new line of the file
+                    outputfile.Write(str);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e) //clear button clicked
+        {
+            foreach (Control c in tabPage1.Controls)
+            {
+                if (c is TextBox)
+                {
+                    if (!(c.Name == "idTxtbx" || c.Name == "jsonfile"))
+                    {
+                        c.Text = "";
+                    }
+                }
+            }
+        }//end of clear button
+
+
+        //===================================================================================================//
+        // There are now methods of the Food Tab
+        //==================================================================================================
+        private void CreateJsonBtn_Click(object sender, EventArgs e)
+        {
+            //FOOD page create button 
+
+            string filepath = foodjsonfile.Text;
+            int dirends = filepath.LastIndexOf(@"\");
+            string dirpath = filepath.Substring(0,++dirends);
+
+            if (Directory.Exists(dirpath))
+            {
+                label22.Visible = false;
+
+                //string jsonBreed;
+                // creates random 4-digit id
+                int seed2 = DateTime.Now.Second;
+                var _random2 = new Random(seed2);
+                int id2 = _random2.Next(1000, 9999);
+                
+				id.Text = id2.ToString();
+                
+                string name = foodnametxtbx.Text;
+                string chemicals = chemicaltxtbx.Text; 
+                string description = descFoodtxt.Text;
+                bool isGood = true;
+                string pic = foodpicfile.Text;
+
+                //building json object string
+
+                // obj = "{ "; curlies were the effin prob all along. Escape them using {{ and }}
+
+                if (radioGood.Checked) isGood = true;
+                else isGood = false;
+                
+                string obj = string.Format(" \r\n{{ \r\n\t\"id\" : {0}, \r\n\t\"name\" : \"{1}\", \r\n\t\"contains\" : \"{2}\", \r\n\t\"isGood\" : {3}, \r\n\t\"description\" : \"{4}\", \r\n\t\"picfile\" : \"{5}\" \r\n }} \r\n , \r\n", id.Text, name,  chemicals, isGood.ToString().ToLowerInvariant(), description, pic);
+
+
+                //obj += "\n\n" + @"}"; //last closing bracket
+
+                writeJson(obj, filepath);
+            }
+            else { label22.Visible = true; return; }
+
+        }
+
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            
+        }//end of clear button
+        
+
+        
+        void ClearBtnClick(object sender, EventArgs e)
+        {
+        	foreach (Control c in tabPage2.Controls)
+            {
+                if (c is TextBox)
+                {
+                    if (!(c.Name == "id" || c.Name == "foodjsonfile"))
+                    {
+                        c.Text = "";
+                    }
+                }
+            }
+        }//end of clear bttn
+    }
+}
